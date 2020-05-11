82756349586b836380555266d7927f1273e24360
tomihaohao <375026495@qq.com>
2020-05-12 03:13:44 +0800

    finish
    计算器 结合 react hook 和 context

---
+++ src/components/NumberProvider.js
@@ -0,0 +1,105 @@
+import React, { useState } from 'react';
+
+//创建Context
+export const NumberContext = React.createContext();
+
+const NumberProvider = props => {
+  const [number, setNumber] = useState('');//数字
+  const [storedNumber, setStoredNumber] = useState('');//存储数字
+  const [functionType, setFunctionType] = useState('');//类型
+
+  const handleSetDisplayValue = num => {
+    if ((!number.includes('.') || num !== '.') && number.length < 8) {
+      setNumber(`${(number + num).replace(/^0+/, '')}`);
+    }
+  };
+
+  const handleSetStoredValue = () => {
+    setStoredNumber(number);
+    setNumber('');
+  };
+
+  //清空操作
+  const handleClearValue = () => {
+    setNumber('');
+    setStoredNumber('');
+    setFunctionType('');
+  };
+
+  const handleBackButton = () => {
+    if (number !== '') {
+      const deletedNumber = number.slice(0, number.length - 1);
+      setNumber(deletedNumber);
+    }
+  };
+
+  const handleSetCalcFunction = type => {
+    if (number) {
+      setFunctionType(type);
+      handleSetStoredValue();
+    }
+    if (storedNumber) {
+      setFunctionType(type);
+    }
+  };
+
+  const handleToggleNegative = () => {
+    if (number) {
+      if (number > 0) {
+        setNumber(`-${number}`);
+      } else {
+        const positiveNumber = number.slice(1);
+        setNumber(positiveNumber);
+      }
+    } else if (storedNumber > 0) {
+      setStoredNumber(`-${storedNumber}`);
+    } else {
+      const positiveNumber = storedNumber.slice(1);
+      setStoredNumber(positiveNumber);
+    }
+  };
+
+  const doMath = () => {
+    if (number && storedNumber) {
+      switch (functionType) {
+        case '+':
+          setStoredNumber(`${Math.round(`${(parseFloat(storedNumber) + parseFloat(number)) * 100}`) / 100}`);
+          break;
+        case '-':
+          setStoredNumber(`${Math.round(`${(parseFloat(storedNumber) - parseFloat(number)) * 1000}`) / 1000}`);
+          break;
+        case '/':
+          setStoredNumber(`${Math.round(`${(parseFloat(storedNumber) / parseFloat(number)) * 1000}`) / 1000}`);
+          break;
+        case '*':
+          setStoredNumber(`${Math.round(`${parseFloat(storedNumber) * parseFloat(number) * 1000}`) / 1000}`);
+          break;
+        default:
+          break;
+      }
+      setNumber('');
+    }
+  };
+
+  return (
+    <NumberContext.Provider
+      value={{
+        doMath,
+        functionType,
+        handleBackButton,
+        handleClearValue,
+        handleSetCalcFunction,
+        handleSetDisplayValue,
+        handleSetStoredValue,
+        handleToggleNegative,
+        number,
+        storedNumber,
+        setNumber,
+      }}
+    >
+      {props.children}
+    </NumberContext.Provider>
+  );
+};
+
+export default NumberProvider;
